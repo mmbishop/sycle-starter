500d9ad891b031e7edccbe13973ebc4e183feb4e
Mike Bishop <tend2dv8@gmail.com>
2022-09-10 00:13:58 -0500

    Added license notice to source files.

--- src/main/java/com/mikebishop/sycle/service/SycleEvaluationService.java
+++ src/main/java/com/mikebishop/sycle/service/SycleEvaluationService.java
@@ -1,3 +1,15 @@
+//<editor-fold desc="Copyright (c) 2022 Michael Bishop">
+// The file is part of Sycle-Starter
+//
+// Sycle-Starter is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+//
+// Sycle-Starter is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with Sycle-Starter. If not, see
+// <https://www.gnu.org/licenses/>.
+//</editor-fold>
 package com.mikebishop.sycle.service;
 
 import com.mikebishop.sycle.language.processor.SycleExpressionEvaluator;
